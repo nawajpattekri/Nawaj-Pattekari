e6f5fd6e762da162d702c61c9f74eec4a2eb2ff7
nawajpattekri <nawajpattekari@gmail.com>
2019-02-15 08:03:56 +0530

    Update calculator.php
    Task 2 - add capability of handling multiple numbers.

--- calculator.php
+++ calculator.php
@@ -1,25 +1,47 @@
 <?php
 	
-	//check user entered command is empty or not
-	if(!empty($argv[1])){
-		if($argv[1]=='sum'){
-			if(!empty($argv[2])){ 
-				if(strpos($argv[2], ",") !== FALSE) {
-				  	$parameters=explode(',', $argv[2]);
-				  	$sum=array_sum($parameters);
-					echo $sum;
-				} else if( is_numeric($argv[2]))  {
-				 	 echo $argv[2];
+	class base_class{
+		
+		function checkCommand($argv){
+			if(!empty($argv[1])){
+				if(!empty($argv[2])){
+
+					if($argv[1]=='sum' || $argv[1]=='add'){
+						$this->makeAddition($argv[2]);
+					}else{
+						echo 'Please provide valied command.';	
+					}
 				}else{
-					echo'please provide valied inputs';
+					echo 'Please provide valied command.';
 				}
+				
+
+			}else{
+				echo 'Please Enter Command.';
 			}
-		}else{
-			echo 'Please Provide Valied Command';
 		}
 
-	}else{
-		echo 'Please Enter Command.';
 	}
 	
+	//class add wich is extends all properties of base_class
+	class add extends base_class
+	{
+		//function foe make additions for entered number
+		function makeAddition($inputs){
+			if(strpos($inputs, ",") !== FALSE) {
+				  	$parameters=explode(',', $inputs);
+				  	$sum=array_sum($parameters);
+					echo $sum;
+				} else if( is_numeric($inputs))  {
+				 	 echo $inputs;
+			}else{
+				echo'please provide valied inputs';
+			}
+		}
+		
+	}
+
+	//create object for class add
+	$addition = new add;
+	echo $addition->checkCommand($argv);
 ?>
